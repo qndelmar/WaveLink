91ede5c0c6e1215d0859f31dbae0c698c8399548
reincarndeity <mihoyodaiteei@gmail.com>
2023-04-29 18:50:56 +0300

    edit hook and render email in component. do decomposition

--- src/pages/AllFriendsList.tsx
+++ src/pages/AllFriendsList.tsx
@@ -7,17 +7,19 @@ import {DocumentData} from "firebase/firestore";
 const AllFriendsList:FC = () => {
     const uid = user().uid || 'none';
     const {kind} = useParams();
-    const [getData,data]= useGetFriends(uid,kind || 'all');
-    const [, setFriendsList] = useState<DocumentData | undefined>();
+    const [getData, data]= useGetFriends(uid,kind || 'all');
+    const [friendsList, setFriendsList] = useState<DocumentData | undefined>();
     async function getFriends(){
         await getData();
-        setFriendsList(data);
     }
     useEffect(() => {
         getFriends();
-    }, [])
+    }, [kind])
     return (
         <div>
+            {data ? data.map((el:DocumentData, i:number) => {
+                return <div key={i}>{el?.email}</div>
+            }) : null}
         </div>
     );
 };
--- src/shared/hooks/useGetFriends.ts
+++ src/shared/hooks/useGetFriends.ts
@@ -1,31 +1,55 @@
-
-import {getDoc, doc, getFirestore, DocumentData} from 'firebase/firestore';
-import {useCallback, useState} from "react";
+import {getDoc, doc, getFirestore, DocumentData, where} from 'firebase/firestore';
+import React, {useCallback, useState, Dispatch, useEffect} from "react";
 
 type FriendsArrayType = DocumentData | undefined;
 type HookReturnedValue = [() => Promise<void>, DocumentData | undefined];
+type getFriendsFunctions = [FriendsArrayType, Dispatch<React.SetStateAction<FriendsArrayType>>]
 
 export function useGetFriends(uid:string, type:string, amount?:number):HookReturnedValue{
-    console.log(amount)
     const [friends, setFriends] = useState<FriendsArrayType>([]);
     const getData = useCallback(
         async ()=> {
-            const friendsList:FriendsArrayType = [];
             if(type === "all"){
-                const data = await getDoc(doc(getFirestore(), "friends", uid))
-                const list = data.data();
-                if(list){
-                    for(const value in list){
-                        const friend = await getDoc(doc(getFirestore(), "users", value.toString()))
-                        friendsList.push(friend.data());
-                    }
-                    setFriends(friendsList);
-                }
-                else{
-                    setFriends(undefined);
-                }
+                await getAllFriends(friends, setFriends, uid);
+                return;
             }
-        }, [type]
+            if(type === "closed"){
+                await getClosedFriends(friends, setFriends, uid);
+                return;
+        }}, [type]
     )
     return [getData, friends];
+}
+
+async function getAllFriends(friends:FriendsArrayType, setFriends:Dispatch<React.SetStateAction<FriendsArrayType>>, uid:string){
+    const friendsList:FriendsArrayType = [];
+    const data = await getDoc(doc(getFirestore(), "friends", uid))
+    const list = data.data();
+    if(list){
+        for(const value in list){
+            const friend = await getDoc(doc(getFirestore(), "users", value.toString()))
+            friendsList.push(friend.data());
+        }
+        setFriends(friendsList);
+        console.log(friends);
+    }
+    else{
+        setFriends(undefined);
+    }
+}
+
+async function getClosedFriends(friends:FriendsArrayType, setFriends:Dispatch<React.SetStateAction<FriendsArrayType>>, uid:string) {
+    const data = await getDoc(doc(getFirestore(), "friends", uid))
+    const friendsList: FriendsArrayType = [];
+    const list = data.data();
+    if (list) {
+        for (let i = 0; i < list.closed.length; i++) {
+            const friend = await getDoc(doc(getFirestore(), "users", list.closed[i].toString()));
+            friendsList.push(friend.data());
+        }
+        setFriends(friendsList);
+    }
+    else {
+        setFriends(undefined);
+    }
 }
